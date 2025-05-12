08e0608d38cb45d495332dde39e816388fb826f3
Encourager <df12376@yandex.ru>
2025-05-12 14:25:16 +0300

    1.4.2

--- src/main/java/dev/enco/greatcombat/utils/colorizer/MinimessageColorizer.java
+++ src/main/java/dev/enco/greatcombat/utils/colorizer/MinimessageColorizer.java
@@ -3,20 +3,11 @@ package dev.enco.greatcombat.utils.colorizer;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 
-import java.util.List;
-
 public class MinimessageColorizer implements ColorizerType {
     @Override
     public String colorize(String message) {
-        if (message == null || message.isEmpty()) {
-            return message;
-        }
         var comp = MiniMessage.miniMessage().deserialize(message);
         return LegacyComponentSerializer.legacySection().serialize(comp);
     }
-
-    @Override
-    public List<String> colorizeAll(List<String> list) {
-        return List.of();
-    }
 }
+
